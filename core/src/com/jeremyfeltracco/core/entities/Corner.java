8a303bf57b4cd263a3eb155b068070fc90ac7e34
jpfeltracco <jpfeltracco@gatech.edu>
2015-10-15 09:52:15 +0200

    Re-added corner code that disappeared.

--- core/src/com/jeremyfeltracco/core/entities/Corner.java
+++ core/src/com/jeremyfeltracco/core/entities/Corner.java
@@ -1,8 +1,16 @@
 package com.jeremyfeltracco.core.entities;
 
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.jeremyfeltracco.core.Textures;
 
-public class Corner {
+public class Corner extends Entity {
+	public Corner(float x, float y) {
+		super(Textures.corner);
+		setOriginPosition(x, y);
+	}
 
+	@Override
+	public void update(float delta) {
+		// TODO Auto-generated method stub
+		
+	}
 }
