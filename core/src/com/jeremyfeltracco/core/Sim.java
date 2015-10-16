08774e24432b0c6cb5fd99a86b93dd4972225f02
knicholson32 <keenanrnicholson@gmail.com>
2015-10-16 09:58:43 +0200

    Finished debugging helper thingy

--- core/src/com/jeremyfeltracco/core/Sim.java
+++ core/src/com/jeremyfeltracco/core/Sim.java
@@ -2,6 +2,7 @@ package com.jeremyfeltracco.core;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -62,8 +63,7 @@ public class Sim extends ApplicationAdapter {
 			controls[i] = new Naive(Side.values()[i], pads, ball);
 		}
 		
-		
-		
+		//---
 		batch = new SpriteBatch();
 		cam = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 		cam.position.x = 0;
@@ -73,6 +73,31 @@ public class Sim extends ApplicationAdapter {
 		Gdx.graphics.setContinuousRendering(true);
 		Gdx.graphics.setVSync(false);
 		//-------
+		//---
+		
+		/*while(!Gdx.input.isKeyPressed(Keys.SPACE)){
+			if(enable){
+				float delta = 0.01666f;//Gdx.graphics.getDeltaTime();
+				systemTime += delta;
+				//world.step(0.1f, 10, 10);
+				for (Controller c : controls)
+					c.update();
+				
+				for(Entity e : Entity.entities)
+					e.update(delta);
+					
+				try {
+					Thread.sleep(0);
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+		
+		System.out.println("EXIT");
+		
+		Gdx.app.exit();*/
 		
 	}
 
@@ -89,15 +114,19 @@ public class Sim extends ApplicationAdapter {
 				e.update(delta);
 		}
 		
-		
 		batch.setProjectionMatrix(cam.combined);
 		Gdx.gl.glClearColor(1, 0.8431372549f, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
 		batch.begin();
+		
 		for(Entity e : Entity.entities)
 			e.draw(batch);
 		
 		batch.end();
+		
+		
+		
 		
 		try {
 			Thread.sleep(Sim.timeDelay);
