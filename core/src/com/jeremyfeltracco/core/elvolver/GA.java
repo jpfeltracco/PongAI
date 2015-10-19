a82805502332d909efde4b0f4bef4ade58a435ab
jpfeltracco <jpfeltracco@gatech.edu>
2015-10-19 09:47:44 +0200

    Finished evolution framework, no debugging yet.

--- core/src/com/jeremyfeltracco/core/elvolver/GA.java
+++ core/src/com/jeremyfeltracco/core/elvolver/GA.java
@@ -1,7 +1,11 @@
 package com.jeremyfeltracco.core.elvolver;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 import org.neuroph.util.random.GaussianRandomizer;
 
+import com.badlogic.gdx.math.MathUtils;
 import com.jeremyfeltracco.core.Sim;
 import com.jeremyfeltracco.core.controllers.MLPerceptronControl;
 import com.jeremyfeltracco.core.entities.Ball;
@@ -20,8 +24,7 @@ public class GA {
 	int gameGrp = 0;
 	int gameNum = 0;
 	
-	double[][] weights;
-	double[] fitness;
+	ArrayList<Element> nets;
 	
 	int genCount = 0;
 	
@@ -29,40 +32,88 @@ public class GA {
 		this.sim = sim;
 		mlps = new MLPerceptronControl[pads.length];
 		r = new GaussianRandomizer(0, 1f);
-		weights = new double[numPerGen][];
+		nets = new ArrayList<Element>();
+
+		for (int i = 0; i < numPerGen; i++)
+			nets.add(new Element());
+		
 		resetFit(); // Initializes and resets fitness
 		
 		// We only need 4 networks, can have weights changed
-		for (int i = 0; i < pads.length; i++) {
+		for (int i = 0; i < pads.length; i++)
 			mlps[i] = new MLPerceptronControl(Side.values()[i], pads, ball);
-		}
 		
 		// Abuse first network to generate 50 arrays of weights
 		for (int i = 0; i < numPerGen; i++) {
 			mlps[0].getNet().randomizeWeights(r);
 			Double[] w = mlps[i].getNet().getWeights();
-			weights[i] = new double[w.length];
-			// Stupid class Double issues
-			for (int j = 0; j < w.length; j++) {
-				weights[i][j] = w[j].doubleValue();
-			}
+			nets.get(i).weights = new double[w.length];
+			// Unbox the doubles
+			for (int j = 0; j < w.length; j++)
+				nets.get(i).weights[j] = w[j].doubleValue();
 		}
 	}
 	
 	public void update(Side loser) {
-		fitness[gameGrp + loser.ordinal()]--; // Group index + loser index
+		nets.get(gameGrp + loser.ordinal()).fitness--; // Group index + loser index
 		gameNum++; // Increase to next game
 		// Check if number of games has exceeded 10
 		if (gameNum >= 10) {
 			gameNum = 0;
-			gameGrp += 4;
+			gameGrp += mlps.length;
 			// Check if all groups in generation are done
 			if (gameGrp > numPerGen - mlps.length) {
 				gameGrp = 0; // Reset
 				curGen++; // Increase generation count
 				// Do something with fitness to make new weights[]
-				double newWeights[] = new double[weights.length];
-				// set weight[0] to highest from previous generation (elitism)
+				Collections.sort(nets); // Lowest index is lowest fitness, higher index is higher fitness
+				@SuppressWarnings("unchecked")
+				ArrayList<Element> newNet = (ArrayList<Element>) nets.clone();
+				int ind1 = 39;
+				for (int i = 0; i < numPerGen; i++) {
+					float rand = MathUtils.random();
+					if (rand > .2f) {
+						if (rand > .4f) {
+							if (rand > .7f) {
+								// pick number in top 35:39
+								ind1 = (int) rand * 5 + 35;
+							} else {
+								// pick number in 30:34
+								ind1 = (int) rand * 5 + 30;
+							}
+						} else {
+							// pick number in 20:29
+							ind1 = (int) rand * 10 + 20;
+						}
+						// pick number in 0:19
+					} else {
+						ind1 = (int) rand * 20;
+					}
+					
+					int ind2 = 39;
+					rand = MathUtils.random();
+					if (rand > .2f) {
+						if (rand > .4f) {
+							if (rand > .7f) {
+								// pick number in top 35:39
+								ind2 = (int) rand * 5 + 35;
+							} else {
+								// pick number in 30:34
+								ind2 = (int) rand * 5 + 30;
+							}
+						} else {
+							// pick number in 20:29
+							ind2 = (int) rand * 10 + 20;
+						}
+						// pick number in 0:19
+					} else {
+						ind2 = (int) rand * 20;
+					}
+					
+					newNet.get(i).weights = reproduce(nets.get(ind1).weights, nets.get(ind2).weights);
+				}
+				
+				
 				// fill in remaining from reproduction algorithm
 				// higher fitness, higher chance to reproduce!
 				
@@ -70,15 +121,23 @@ public class GA {
 			}
 			// Set weights for next group
 			for (int i = 0; i < mlps.length; i++)
-				mlps[i].getNet().setWeights(weights[gameGrp + i]);
+				mlps[i].getNet().setWeights(nets.get(gameGrp + i).weights);
 		}
 		// Continue playing!
 	}
 	
+	private double[] reproduce(double[] one, double[] two) {
+		double[] child = new double[one.length];
+		for (int i = 0; i < child.length / 2; i++)
+			child[i] = one[i];
+		for (int i = child.length / 2; i < child.length; i++)
+			child[i] = two[i];
+		return child;
+	}
+	
 	private void resetFit() {
-		if (fitness == null) fitness = new double[numPerGen];
-		for (int i = 0; i < fitness.length; i++)
-			fitness[i] = numGamesPer;
+		for (Element net : nets)
+			net.fitness = numGamesPer;
 	}
 	
 }
